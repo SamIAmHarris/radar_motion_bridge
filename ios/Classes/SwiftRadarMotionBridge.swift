931f07b02acda16c5a447a39d52daa14bc0ad774
samiamharris <samharris512@gmail.com>
2025-09-22 12:59:34 -0400

    Enable motion tracking from dashboard. Make this a no-op plugin just to bring in correct motion dependency

--- ios/Classes/SwiftRadarMotionBridge.swift
+++ ios/Classes/SwiftRadarMotionBridge.swift
@@ -3,33 +3,9 @@
 //
 import Flutter
 import UIKit
-import RadarSDK
 
  public class SwiftRadarMotionBridge: NSObject, FlutterPlugin {
    public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "radar_motion_bridge",
-                                       binaryMessenger: registrar.messenger())
-    let instance = SwiftRadarMotionBridge()
-    registrar.addMethodCallDelegate(instance, channel: channel)
+    //no-op plugin exists only to bring in CocoaPods for RadarMotion
    }
-
-   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "enableLocationMetadata":
-      // Depending on Radar iOS SDK version, the configuration API is one of these shapes.
-      // Keep the one that compiles for your version:
-      DispatchQueue.main.async {
-
-        // Set a new configuration object
-        let cfg = RadarSdkConfiguration()
-        cfg.useLocationMetadata = true
-        Radar.setSdkConfiguration(cfg)
-
-        result(nil)
-      }
-
-    default:
-      result(FlutterMethodNotImplemented)
-    }
-  }
 }
