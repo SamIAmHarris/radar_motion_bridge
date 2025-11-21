4160f9cb1080cb515ecc7bf2ad62c0486940d459
samiamharris <samharris512@gmail.com>
2025-11-21 12:26:31 -0500

    Add pubspec.yaml android flow

---
+++ android/src/main/kotlin/com/redrhinoresults/radarmotionbridge/RadarMotionBridgePlugin.kt
@@ -0,0 +1,8 @@
+package com.redrhinoresults.radarmotionbridge
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+
+class RadarMotionBridgePlugin : FlutterPlugin {
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {}
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {}
+}
