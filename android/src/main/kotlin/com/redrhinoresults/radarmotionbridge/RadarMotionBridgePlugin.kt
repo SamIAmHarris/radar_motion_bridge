6fef83dd53c1980850534b8ab54403844e68e586
samiamharris <samharris512@gmail.com>
2025-11-21 12:42:19 -0500

    Add all the necessary android files

--- android/src/main/kotlin/com/redrhinoresults/radarmotionbridge/RadarMotionBridgePlugin.kt
+++
@@ -1,8 +0,0 @@
-package com.redrhinoresults.radarmotionbridge
-
-import io.flutter.embedding.engine.plugins.FlutterPlugin
-
-class RadarMotionBridgePlugin : FlutterPlugin {
-    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {}
-    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {}
-}
